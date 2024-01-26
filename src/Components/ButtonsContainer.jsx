6687b237e17a6b2cb6e49506f79611d957dc3f20
Shameer Mukhtar <92436805+IamShameerMukhtar@users.noreply.github.com>
2024-01-26 22:35:09 +0500

    Calculator Using React

---
+++ src/Components/ButtonsContainer.jsx
@@ -0,0 +1,13 @@
+import styles from "./ButtonsContainer.module.css"
+const ButtonsContainer=()=>{
+const ButtonNames=['c','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0',]
+
+
+
+return <div id={styles.button_Container}>
+{ButtonNames.map(names=><button className={styles.button1}>{names}</button>)}
+
+</div>
+
+}
+export default ButtonsContainer
---
+++ src/Components/Display.jsx
@@ -0,0 +1,7 @@
+import styles from './Display.module.css'
+const Display=()=>{
+  return<div>
+<input type='number' className={styles.display}></input>
+</div>
+}
+export default Display
